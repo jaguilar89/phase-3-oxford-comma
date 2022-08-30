34b1a1988d1030065d0c2df688e13acedbd1bd25
Jose Aguilar <jaguilarny@gmail.com>
2022-08-30 00:23:49 -0400

    commit tree ac8fb58150d62dae030b995013e521a5a8e2f04e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length > 2
+        first_elements = array.slice(0..-2)
+        last_element = "and " + array.slice(-1)
+        combined_arrays = first_elements << last_element
+        combined_arrays.join(", ")
+    elsif array.length == 2
+        "#{array[0]} and #{array[1]}"
+    else
+        array[0]
+    end
 end
+
+oxford_comma(['moe', 'bob', 'curly', 'beavis', 'butthead'])
+
